903069e9a0e0133d7f6927161fc557dde950ef09
1i6l29873 <72370602+1i6l29873@users.noreply.github.com>
2021-11-04 11:27:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,6 +28,9 @@ sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$f1oZaeV
 
 # Add app
 svn co https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+# 全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
+# 用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # svn co https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 # git clone https://github.com/destan19/OpenAppFilter package/lean/luci-app-oaf
 
